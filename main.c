20db60559c0e106c0e5b78e3b4e4a8267412cb05
heesun-hong <dukye2001@naver.com>
2019-11-28 16:08:54 +0900

    third

--- main.c
+++ main.c
@@ -3,12 +3,61 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int is_whitespace(char c)
+{
+	if(c == ' ' ||
+	   c == '\n' ||
+       c == '\r' ||
+	   c == '\t' ||
+	   c == '(')
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+int fget_word(FILE* fp, char *word)
+{
+	int cnt;
+	char c;
+	
+	while( (c = fgetc(fp)) != EOF)
+	{
+		if (is_whitespace(c)==0)
+		break;
+	}
+	
+	if (c==EOF)
+	{
+		return 0;
+	}
+	
+	cnt=0;
+	word[cnt++] = c;
+	word[cnt] = '\0';
+	
+	while ( (word[cnt] = fgetc(fp)) != EOF)
+	{
+		if (is_whitespace(word[cnt]) == 1)
+		{
+			word[cnt] = '\0';
+			break;
+		}
+		cnt ++;
+	}
+}
+
+
+
+
 
 
 int main(int argc, char *argv[]) {
 	
 	FILE *fp;
 	char filepath[100];
+	char word[100];
 	
 	//file open
 	printf("input the file path: ");
@@ -20,10 +69,13 @@ int main(int argc, char *argv[]) {
 		printf("file path is wrong! %s\n", filepath);
 		return -1;
 	}
-	fclose(fp);
-	//word reading &analysis
-	
+	//word reading & analysis
+	while (fget_word(fp, word) !=0)
+	{
+		printf("%s\n",word);
+	}
 	//output
-	
+	fclose(fp);
+
 	return 0;
 }
